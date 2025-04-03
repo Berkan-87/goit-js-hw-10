699cc72a10112e54cba688227e4826f2fdd6ab68
Berkan-87 <sby.notess@gmail.com>
2025-04-03 18:25:34 +0300

    goit-js-hw

--- vite.config.js
+++ vite.config.js
@@ -4,49 +4,52 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    base: '/goit-js-hw-10/',
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+export default defineConfig({
+  base: '/goit-js-hw-10/',
+  resolve: {
+    alias: {
+      flatpickr: 'flatpickr', // flatpickr modülünü doğrudan çözümle
     },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/index.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+  },
+  define: {
+    global: {},
+  },
+  root: 'src',
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: glob.sync('./src/index.html'),
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames: chunkInfo => {
+          if (chunkInfo.name === 'commonHelpers') {
+            return 'commonHelpers.js';
+          }
+          return '[name].js';
+        },
+        assetFileNames: assetInfo => {
+          if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+            return '[name].[ext]';
+          }
+          return 'assets/[name]-[hash][extname]';
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
-    server: {
-      open: true,
-    },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/*.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  server: {
+    open: true,
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/*.html']),
+    SortCss({
+      sort: 'mobile-first',
+    }),
+  ],
 });
